e7a2927709511ca37d4f5192546659f3f8979fb8
﻿คʕ•ﻌ•ʔค <89661121+Moonsu-11@users.noreply.github.com>
2024-05-08 23:42:41 +0900

    FEAT : 개구리 배 자르기
    개구리 배 자르기
    라인을 만들어서 칼이 닿으면 색이 변하고 배가 잘린 모습으로 변경됨.

---
+++ Assets/Scripts/CuttingFrog.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingFrog : MonoBehaviour
+{
+    public Transform frogPosition;
+    public GameObject CuttingLine1;
+    public GameObject CuttingLine2;
+    public GameObject CuttingLine3;
+    public GameObject CuttingLine4;
+    public GameObject CuttingLine5;
+    public GameObject CuttingLine6;
+    public GameObject Knife;
+    public GameObject BeforeFrog;
+    public GameObject AfterFrog;
+    private int count = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Line1"))
+        {
+            var LineRenderer1 = CuttingLine1.GetComponent<Renderer>();
+            LineRenderer1.material.SetColor("_Color", Color.red);
+            Debug.Log(count);
+            count = 2;
+        }
+        if (collision.gameObject.CompareTag("Line2"))
+        {
+            var LineRenderer2 = CuttingLine2.GetComponent<Renderer>();
+            LineRenderer2.material.SetColor("_Color", Color.red);
+            Debug.Log(count);
+            count = 3;
+        }
+        if (collision.gameObject.CompareTag("Line3"))
+        {
+            var LineRenderer3 = CuttingLine3.GetComponent<Renderer>();
+            LineRenderer3.material.SetColor("_Color", Color.red);
+            Debug.Log(count);
+            count = 4;
+        }
+        if (collision.gameObject.CompareTag("Line4"))
+        {
+            var LineRenderer4 = CuttingLine4.GetComponent<Renderer>();
+            LineRenderer4.material.SetColor("_Color", Color.red);
+            Debug.Log(count);
+            count = 5;
+        }
+        if (collision.gameObject.CompareTag("Line5"))
+        {
+            var LineRenderer5 = CuttingLine5.GetComponent<Renderer>();
+            LineRenderer5.material.SetColor("_Color", Color.red);
+            Debug.Log(count);
+            count = 6;
+        }
+        if (collision.gameObject.CompareTag("Line6"))
+        {
+            var LineRenderer6 = CuttingLine6.GetComponent<Renderer>();
+            LineRenderer6.material.SetColor("_Color", Color.red);
+            Debug.Log(count);
+            AllLineRed();
+        }
+    }
+
+    private void AllLineRed()
+    {
+        StartCoroutine(FlashLines());
+    }
+
+    private IEnumerator FlashLines()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            // 1초 동안 모든 선이 투명색이 됩니다.
+            SetLineColor(Color.clear);
+            yield return new WaitForSeconds(1f);
+
+            // 1초 동안 모든 선이 빨간색이 됩니다.
+            SetLineColor(Color.red);
+            yield return new WaitForSeconds(1f);
+        }
+
+        // 모든 선이 빨간색이 된 후 Cut() 함수를 호출합니다.
+        Cut();
+    }
+
+    private void SetLineColor(Color color)
+    {
+        CuttingLine1.GetComponent<Renderer>().material.SetColor("_Color", color);
+        CuttingLine2.GetComponent<Renderer>().material.SetColor("_Color", color);
+        CuttingLine3.GetComponent<Renderer>().material.SetColor("_Color", color);
+        CuttingLine4.GetComponent<Renderer>().material.SetColor("_Color", color);
+        CuttingLine5.GetComponent<Renderer>().material.SetColor("_Color", color);
+        CuttingLine6.GetComponent<Renderer>().material.SetColor("_Color", color);
+    }
+
+    private void Cut()
+    {
+        Destroy(BeforeFrog);
+        Instantiate(AfterFrog, frogPosition.position, frogPosition.rotation);
+    }
+}
