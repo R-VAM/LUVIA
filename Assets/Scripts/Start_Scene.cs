9efaa6d5556976d314f1c4a2fa239e79a4723a20
Dasol <65291202+ekthf2013@users.noreply.github.com>
2024-05-16 03:24:07 +0900

    FEAT: 시작화면 Sceen 생성 및 고정핀 기능 수정
    시작화면 Sceen 따로 생성, 전환 기능 고정핀 기능 보완

--- Assets/Scripts/Start_Scene.cs
+++ Assets/Scripts/Start_Scene.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Start_Scene : MonoBehaviour
-{
-    public Canvas canvasToDisable; // 비활성화할 캔버스
+public class Start_Scene : MonoBehaviour {
+
+    //public Button button;
+    //public Canvas canvasToDisable;
+
+    void Start()
+    {
+/*        button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClick);*/
+    }
+
+/*    void OnButtonClick()
+    {
+        SceneManager.LoadScene("LabScene");
+    }*/
 
     private void OnTriggerEnter(Collider collision)
     {
-        canvasToDisable.gameObject.SetActive(false);
+        //canvasToDisable.gameObject.SetActive(false);
+        SceneManager.LoadScene("Scene_Laboratory");
     }
+
 }
